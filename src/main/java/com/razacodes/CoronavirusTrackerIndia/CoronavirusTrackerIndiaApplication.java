e33f2eddc826403bae02c19b161e2ab25d299f2c
razaaasif <aasifraza9123@gmail.com>
2021-05-20 21:57:31 +0530

    Created starter spring-boot project

---
+++ src/main/java/com/razacodes/CoronavirusTrackerIndia/CoronavirusTrackerIndiaApplication.java
@@ -0,0 +1,13 @@
+package com.razacodes.CoronavirusTrackerIndia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusTrackerIndiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusTrackerIndiaApplication.class, args);
+	}
+
+}
